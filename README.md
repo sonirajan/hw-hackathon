52ded8fa7613ea2790c7bd982c57cb4d755720af
Rajan Soni <rsoni@hotwire.com>
2016-01-27 11:16:21 -0800

    24 hr hackathon done - use can login to his laptop by scanning qr code through his logged-in mobile device

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Topic: Password less login if user is already logged in to one of her mobile dev
 How to use:
 1) npm install socket.io
 2) node server.js
+3) open in your laptop: http://sfoc02q47ujg8wn.local:8001/login.html
+4) open in your cell phone: http://sfoc02q47ujg8wn.local:8001/login.html
+   login with admin/admin
+   Click choose file button, take qr code photo from laptop and you will be automatically logged in to mac.
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var server;
 
 server = http.createServer(function(req, res){
-    // your normal server code
     var path = url.parse(req.url).pathname;
     switch (path){
         case '/':
@@ -13,22 +12,54 @@ server = http.createServer(function(req, res){
             res.end();
             break;
         case '/test.html':
+        case '/login.html' :
+        case '/qrCodeReader.html' :
+        case '/welcome.html' :
             fs.readFile(__dirname + path, function(err, data){
-                if (err){ 
+                if (err){
                     return send404(res);
                 }
                 res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
                 res.write(data, 'utf8');
                 res.end();
             });
-        break;
+            break;
+        case '/socket.io-1.3.7.js':
+        case '/jquery.js':
+        case '/qrcode.js':
+        case '/src/grid.js':
+        case '/src/version.js':
+        case '/src/detector.js':
+        case '/src/formatinf.js':
+        case '/src/errorlevel.js':
+        case '/src/bitmat.js':
+        case '/src/datablock.js':
+        case '/src/bmparser.js':
+        case '/src/datamask.js':
+        case '/src/rsdecoder.js':
+        case '/src/gf256poly.js':
+        case '/src/gf256.js':
+        case '/src/decoder.js':
+        case '/src/qrcode.js':
+        case '/src/findpat.js':
+        case '/src/alignpat.js':
+        case '/src/databr.js':
+            fs.readFile(__dirname + path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.write(data, 'utf8');
+                res.end();
+            });
+            break;
         default: send404(res);
     }
-}),
+});
 
 send404 = function(res){
     res.writeHead(404);
-    res.write('404');
+    res.write('404 PAGE NOT FOUND');
     res.end();
 };
 
@@ -40,8 +71,12 @@ var io = require('socket.io').listen(server);
 //turn off debug
 io.set('log level', 1);
 
+var arrClientIds = [];
+
 // define interactions with client
 io.sockets.on('connection', function(socket){
+
+    /// temp practice code for temp.html
     //send data to client
     setInterval(function(){
         socket.emit('date', {'date': new Date()});
@@ -51,4 +86,17 @@ io.sockets.on('connection', function(socket){
     socket.on('client_data', function(data){
         process.stdout.write(data.letter);
     });
+    /////////////////////
+
+    socket.on('onClientIdGeneration', function(data){
+        arrClientIds.push(data);
+        console.log(arrClientIds);
+        socket.emit('allClientIds', data);
+    });
+
+    socket.on('onDecodingQRCode', function(data){
+        console.log('onDecodingQRCode: from: ' + data.from);
+        io.sockets.emit('onLoginRequest', data);
+    });
+
 });
